4eea7f2a21ebc0956578a03bf5fc372a7cf6a39e
lidogen <aleksejraduk1820@gmail.com>
2020-05-29 18:34:16 +0300

    lesson-6

--- Students/iessei-developer/project/webpack.config.js
+++ Students/iessei-developer/project/webpack.config.js
@@ -1,68 +1,143 @@
 const path = require('path');
-const miniCssExtractPlugin = require('mini-css-extract-plugin');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
-	entry:
-	'./src/index.js',
-
-	output: {
-
-		path: path.resolve(__dirname, 'dist'),
-
-		publicPath: 'dist',
-
-		filename: 'main1.js'
-
-	},
-
-	module: {
-
-		rules: [
-			{
-				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			},
-			{
-				test: /\.css$/i,
-				use: [
-					{
-						loader: miniCssExtractPlugin.loader,
-						options: {
-							esModule: true,
-						},
-					},
-					'css-loader',
-				],
-			},
-
-	]
-},
-
-devServer: {
-	overlay: true
-},
-
-
-plugins: [
-	new miniCssExtractPlugin({
-		filename: 'css/main.css',
-		chunkFilename: '[id].css',
-	}),
-],
-plugins: [
-	new htmlWebpackPlugin({
-		template:'src/public/index.html'
-	}),
-]
-
-
-
+    entry: {
+        main: ["@babel/polyfill", "./src/public/index.js"]
+    },
+    output: {
+        path: path.join(__dirname, 'dist/public'),
+        publicPath: "/",
+        filename: "js/[name].js"
+    },
+    target: 'web',
+    devtool: "#source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/public/index.html',
+            filename: 'index.html',
+            excludeChunks: ['server']
+        })
+    ]
 };
+// const path = require('path');
+// const miniCssExtractPlugin = require('mini-css-extract-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// // const compiler = require ('vue-template-compiler');
+// const VueLoaderPlugin = require('vue-loader-plugin');
+//
+//
+//
+// module.exports = {
+//
+// 	entry:
+// 	'./src/public/index.js',
+//
+// 	output: {
+//
+// 		path: path.resolve(__dirname, 'dist'),
+//
+// 		publicPath: 'dist',
+//
+// 		filename: 'main1.js'
+//
+// 	},
+//
+// 	module: {
+//
+// 		rules: [
+// 			{
+// 				test: /\.m?js$/,
+// 				exclude: /(node_modules|bower_components)/,
+// 				use: {
+// 					loader: 'babel-loader',
+// 					options: {
+// 						presets: ['@babel/preset-env']
+// 					}
+// 				}
+// 			},
+//
+// 			{
+//                 test: /\.html$/,
+//                 use: [
+//                     {
+//                         loader: "html-loader"
+//                     }
+//                 ]
+//             },
+//
+// 			{
+// 				test: /\.vue$/,
+// 				loader: 'vue-loader'
+// 			},
+//
+// 			// {
+// 			// 	test: /\.css$/i,
+// 			// 	use: [
+// 			// 		{
+// 			// 			loader: miniCssExtractPlugin.loader,
+// 			// 			options: {
+// 			// 				esModule: true,
+// 			// 			},
+// 			// 		},
+// 			// 		'css-loader',
+// 			// 	],
+// 			// },
+// 			{
+//                 test: /\.css$/,
+//                 use: ['style-loader', 'css-loader']
+//             },
+//
+// 		]
+// 	},
+//
+// devServer: {
+// 	overlay: true
+// },
+//
+//
+// plugins: [
+// 	new miniCssExtractPlugin({
+// 		filename: 'css/main.css',
+// 		chunkFilename: '[id].css',
+// 	}),
+// ],
+// plugins: [
+// 	new HtmlWebpackPlugin({
+// 		template:'src/public/index.html',
+// 		filename: 'index.html'
+// 	}),
+// ],
+// plugins: [
+//     new VueLoaderPlugin()
+// ]
+//
+//
+//
+//
+// };
