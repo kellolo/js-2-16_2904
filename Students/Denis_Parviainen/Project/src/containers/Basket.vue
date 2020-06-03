7d8c588c7c06fdfaa40cf96d37785a2ebb2852f7
Flopyk <flopyk@yandex.ru>
2020-06-03 14:37:43 +0300

    'seventh'

---
+++ Students/Denis_Parviainen/Project/src/containers/Basket.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="cart-block ">
+        <div class="d-flex">
+            <strong class="d-block">Всего товаров</strong>
+            <div id="quantity"></div>
+        </div>
+        <hr>
+        <div class="cart-items">
+            <item v-for="item of items" :key="item.id_product"/>
+        </div>
+        <hr>
+        <div class="d-flex">
+            <strong class="d-block">Общая ст-ть:</strong>
+            <div id="price"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import item from '../components/Item.vue'
+    export default {
+        components: { item },
+        data() {
+            return {
+                items: [],
+                url: '',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
