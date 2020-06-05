cf0c5d42c86164201c905f8c0e72cf988ff982c9
Denis <pro166@mail.ru>
2020-06-05 20:47:52 +0300

    ДЗ 7

---
+++ Students/Shubin Denis/Project/src/containers/Cart.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="cart-block ">
+        <div class="d-flex">
+            <strong class="d-block">Всего товаров</strong>
+            <div id="quantity"></div>
+        </div>
+        <hr>
+        <div class="cart-items">
+            <item v-for="item of items" :key="item.id_product" :type="'cart'" :item="item"/>
+        </div>
+        <hr>
+        <div class="d-flex">
+            <strong class="d-block">Общая ст-ть:</strong>
+            <div id="price"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Item from '../components/Item.vue';
+export default {
+    components: { Item },
+    data() {
+        return {
+            items: [],
+            url: '/api/cart',
+        }
+    },
+    mounted() {
+        this.$parent.get(this.url).then(data => {
+            this.items = data.contents;
+        })
+    },
+    methods: {
+        addProduct(item) {
+            let find = this.items.find(elem => elem.id_product === item.id_product);
+            if (find) {
+                find.quantity++;
+                } else {
+                let item1 = Object.assign({quantity: 1}, item);
+                this.items.push(item1);
+            }
+        },
+        deleteProduct(item) {
+            if (item.quantity > 1) {
+                item.quantity--;           
+            } else {
+                this.items.splice(this.items.indexOf(item), 1);
+            }
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
--- Students/Shubin Denis/Project/src/views/Shop.vue
+++ Students/Shubin Denis/Project/src/views/Shop.vue
@@ -1,38 +1,58 @@
-  
 <template>
-  <div>
-    <header>
-      <div class="logo">E-shop</div>
-      <div class="cart">
-        <search />
-        <button @click="showBasket=!showBasket" class="btn-cart">Cart</button>
-        <Basket ref="basket" v-show="showBasket" />
-      </div>
-    </header>
-    <main>
-      <catalog />
-    </main>
-  </div>
+    <div>
+        <header>
+            <div class="logo">E-shop</div>
+            <div class="cart">
+                <form action="#" class="search-form" >
+                    <input type="text" class="search-field" >
+                    <button class="btn-search">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+                <button class="btn-cart" type="button" @click="showCart = !showCart">Cart</button>
+                <Cart ref="cart" v-show="showCart"/>
+            </div>
+        </header>
+        <main>
+            <Catalog @addProduct="addItem"/>
+        </main>  
+    </div> 
 </template>
 
 <script>
-import Basket from "../containers/Basket.vue";
-import Catalog from "../containers/Catalog.vue";
-import Search from "../components/Search.vue";
+import Cart from '../container/Cart.vue'
+import Catalog from '../container/Catalog.vue'
 export default {
-  components: { Basket, Catalog, Search },
-  data() {
-    return {
-      showBasket: true
-    };
-  },
-  methods: {
-    get(url) {
-      return fetch(url).then(data => data.json());
-    }
-  }
-};
+    components: { Cart, Catalog },
+    data() {
+        return {showCart: false}
+    },
+    methods: {
+        get(url) {
+            return fetch(url).then(data => data.json())
+        },
+        post(url, item) {
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(item)
+            })
+        },
+        put() {
+
+        },
+        delete() {
+
+        },
+        addItem(pl) {
+            this.$refs.cart.addProduct(pl);
+        }
+    },
+}
 </script>
 
 <style>
+
 </style>
