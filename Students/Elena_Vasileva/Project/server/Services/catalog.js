7cc42a5664734bd4562efcbcf4ecd3b3f83e0dcb
Kleossa <kleossa@yandex.ru>
2020-06-12 13:28:43 +0700

    DZ 8 часть 2.

--- Students/Elena_Vasileva/Project/server/Services/catalog.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    add(catalog, item) {
-        item.id_product = Date.now() + '';
-        catalog.push(item);
-        return { newCatalog: catalog, idNewItem: item.id_product };
-    }
-};
